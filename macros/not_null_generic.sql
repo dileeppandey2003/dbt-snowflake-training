6da096daf83dcd61a8fbcc3c996c8b55e90d2dc9
Dileep Pandey <dk7007354@gmail.com>
2026-01-21 04:03:16 +0000

    I created a reusable macro

--- macros/not_null_generic.sql
+++
@@ -1,7 +0,0 @@
-{% test not_null_generic(model, column_name) %}
-
-select *
-from {{ model }}
-where {{ column_name }} is null
-
-{% endtest %}
--- models/customers_view.sql
+++ models/customers_view.sql
@@ -1,8 +1,9 @@
-SELECT
-  customer_id,
-  first_name,
-  last_name,
-  email,
-  city,
-  created_at
-FROM {{ source('raw', 'customers') }}
+select
+    customer_id,
+    {{ to_upper('first_name') }} as first_name,
+    {{ to_upper('last_name') }}  as last_name,
+    email,
+    city,
+    created_at
+from {{ source('raw', 'customers') }}
+
